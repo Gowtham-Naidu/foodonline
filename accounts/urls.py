a01b731744c07630f68813a03979550df995024e
rock <gowthamnaiducsk@gmail.com>
2023-02-18 22:20:50 +0530

    customer and vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('registeruser/', views.registeruser,name='registeruser'),
     path('registervendor/', views.registervendor,name='registervendor'),
+    
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('myAccount/',views.myAccount,name='myAccount'),
+    path('custDashboard/',views.custDashboard,name='custDashboard'),
+    path('vendorDashboard/',views.vendorDashboard,name='vendorDashboard')
 ]
---
+++ accounts/utils.py
@@ -0,0 +1,10 @@
+#helper functions
+def detectUser(user):
+    if user.role == 1:
+        redirectUrl = 'vendorDashboard'
+        return redirectUrl
+    elif user.role == 2:
+        redirectUrl = 'custDashboard'
+        return redirectUrl
+    elif user.role == None and user.is_superadmin:
+        redirectUrl = '/admin'
