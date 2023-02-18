7890948f9ee0bf9cb125f21bd5a22096913e3f9a
rock <gowthamnaiducsk@gmail.com>
2023-02-18 09:39:04 +0530

    register user functionalities and the homepage setup

---
+++ accounts/signals.py
@@ -0,0 +1,25 @@
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+from .models import User,UserProfile
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender,instance,created,**Kwargs):
+    print(created)
+    if created:
+        UserProfile.objects.create(user=instance)
+        print('user profile is created')
+    
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            # create the user profile if not exist
+            UserProfile.objects.create(user=instance)
+            print('profile was not exist but i created one')
+        print('user is updated')
+#post_save.connect(post_save_create_profile_receiver,sender=User)
+
+@receiver(pre_save, sender=User)    
+def pre_save_profile_receiver(sender, instance, **kwargs):
+    print(instance.username,'this user is being saved')
--- foodonline/settings.py
+++ foodonline/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -128,8 +130,17 @@ STATICFILES_DIRS=[
     'foodonline/static'
 ] 
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+    
+}
